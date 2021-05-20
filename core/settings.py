a41951edbaa7d8453460ba4db7ad6ee3aa6c62be
rrojan20 <rrojan20@gmail.com>
2021-05-20 13:07:24 +0545

    added feature: throttling

--- core/settings.py
+++ core/settings.py
@@ -137,5 +137,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '0/day',
+        'user': '120/day'
+    }
 }
